3b099a4da5d4839953ba747698c4b95311060d62
pandarudra <rudrapanda8206@gmail.com>
2025-07-19 16:14:58 +0530

    twillo

--- apps/server/src/utils/sms.ts
+++ apps/server/src/utils/sms.ts
@@ -1,3 +1,14 @@
+import twilio from "twilio";
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID!;
+const authToken = process.env.TWILIO_AUTH_TOKEN!;
+const twilioPhone = process.env.TWILIO_PHONE_NUMBER!;
+const client = twilio(accountSid, authToken);
+
 export const sendOtpSms = async (phoneNumber: string, otp: string) => {
-  console.log(`Sending OTP ${otp} to phone number ${phoneNumber}`);
+  await client.messages.create({
+    body: `Your ChitChat OTP is: ${otp}`,
+    from: twilioPhone,
+    to: phoneNumber,
+  });
 };
